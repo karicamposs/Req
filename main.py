b367716d03b11f97038958041293805ce3d83373
Karina Campos <147186747+karicamposs@users.noreply.github.com>
2024-06-10 19:21:51 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from ScraperClass import Scraper
+import pandas as pd
+
+# Uso de la clase
+url = 'https://www.bbc.com/mundo/topics/c7zp57yyz25t'
+scraper = Scraper(url)
+news_links = scraper.run(18)
+
+
+df = pd.read_csv("datos_noticias.csv", sep=",")
+print(df)
